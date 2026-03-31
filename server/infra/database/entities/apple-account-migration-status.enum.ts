80f76655140bf373b68b8bf8e1a8f02f35834bff
dennis0405 <dennis0405@snu.ac.kr>
2026-03-31 20:52:29 +0900

    chore: support apple sign in migration

---
+++ server/infra/database/entities/apple-account-migration-status.enum.ts
@@ -0,0 +1,7 @@
+export enum AppleAccountMigrationStatus {
+  PENDING = 'pending',
+  TRANSFER_IDENTIFIER_COLLECTED = 'transfer_identifier_collected',
+  NEW_SUB_COLLECTED = 'new_sub_collected',
+  MIGRATED = 'migrated',
+  FAILED = 'failed',
+}
