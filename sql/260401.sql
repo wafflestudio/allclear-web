edbcb3696de65a65371f33fc08bce2ea07926a5f
양해울 <haeul1005@naver.com>
2026-04-01 21:04:48 +0900

    refactor: club테이블의 affiliation_type 값 채우기

--- sql/260401.sql
+++ sql/260401.sql
@@ -9,3 +9,11 @@ SELECT college, NULL
 FROM college_major
 GROUP BY college
 HAVING COUNT(*) > 1;
+
+UPDATE club
+SET affiliation_type = CASE 
+    WHEN college IN ('단과대무관', '법과대학') THEN '기타'
+    WHEN college = '연합동아리' THEN '연합동아리'
+    WHEN college = '중앙동아리' THEN '중앙동아리'
+    ELSE '소속동아리'
+END;
