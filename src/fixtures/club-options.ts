d1c4e9a7f494fd85979d3b3b2dd389f5098cc710
Seol Handong <yabsed@snu.ac.kr>
2026-04-02 02:05:37 +0900

    feature: club의 새로운 두 필드 afflication_type과 college_major_id (#16)

---
+++ src/fixtures/club-options.ts
@@ -0,0 +1,37 @@
+export const CLUB_COLLEGES = [
+  '단과대무관',
+  '중앙동아리',
+  '연합동아리',
+  '사범대학',
+  '자연과학대학',
+  '농업생명과학대학',
+  '경영대학',
+  '간호대학',
+  '공과대학',
+  '인문대학',
+  '생활과학대학',
+  '음악대학',
+  '사회과학대학',
+  '수의과대학',
+  '미술대학',
+  '법과대학',
+  '약학대학',
+  '의과대학',
+  '자유전공대학',
+] as const
+
+export const CLUB_CATEGORIES = [
+  '취미',
+  '공연',
+  '홍보',
+  '진로',
+  '종교',
+  '문화',
+  '학술',
+  '운동',
+  '봉사',
+] as const
+
+export const CLUB_RECRUIT_TYPES = ['정기', '상시', '매 학기', '매 년'] as const
+
+export const CLUB_AFFILIATION_TYPES = ['기타', '연합동아리', '중앙동아리', '소속동아리'] as const
