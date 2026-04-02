740f3fba9f928dc745ec1d07ab585c99b344e931
양해울 <haeul1005@naver.com>
2026-04-03 00:07:20 +0900

    feature: 현재 등록된 모든 동아리를 APPROVED 상태로 만드는 SQL문

--- sql/260402.sql
+++ sql/260402.sql
@@ -2,3 +2,5 @@ ALTER TABLE club DROP COLUMN is_approved;
 ALTER TABLE club 
     ADD COLUMN status character varying NOT NULL DEFAULT 'PENDING',
     ADD COLUMN reject_reason character varying DEFAULT '';
+
+UPDATE club SET status = 'APPROVED';
