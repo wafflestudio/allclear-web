031ab02a83f4a282b7a348b941541c389931ed1b
양해울 <haeul1005@naver.com>
2026-04-03 00:01:17 +0900

    feature: 동아리 승인/반려 관련 SQL문

---
+++ sql/260402.sql
@@ -0,0 +1,4 @@
+ALTER TABLE club DROP COLUMN is_approved;
+ALTER TABLE club 
+    ADD COLUMN status character varying NOT NULL DEFAULT 'PENDING',
+    ADD COLUMN reject_reason character varying DEFAULT '';
