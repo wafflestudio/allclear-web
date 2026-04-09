6c206be991273032073d14eb012a0792298f4d19
Nam Hee Kim <103100783+kimnamheeee@users.noreply.github.com>
2026-04-09 16:28:00 +0900

    Revert "chore: update privacy policy and terms of service (#27)"
    This reverts commit 21c883c15c8512943efddb3abeff483244578c1c.

--- pages/terms/privacy-contact-section-kr.tsx
+++ pages/terms/privacy-contact-section-kr.tsx
@@ -2,12 +2,12 @@ const PrivacyContactSectionKr = () => (
   <section className="prose mb-8">
     <h2 className="mb-4 text-xl font-bold">개인정보관리책임자 및 담당자</h2>
     <span>
-      WaffleStudio는 회원의 개인정보보호를 가장 중요시하며, 회원의 개인정보가 훼손, 침해 또는
-      누설되지 않도록 최선을 다하고 있습니다.
+      신선한여울은 회원의 개인정보보호를 가장 중요시하며, 회원의 개인정보가 훼손, 침해 또는 누설되지
+      않도록 최선을 다하고 있습니다.
     </span>
     <h4>[개인정보관리책임자]</h4>
     <span>
-      올클 담당자 전자우편 : <span className="font-semibold">dev.clubhouse@gmail.com</span>
+      신선한여울 담당자 전자우편 : <span className="font-semibold">support@theplato.io</span>
     </span>
     <p className="prose whitespace-pre-wrap">
       {`기타 개인정보에 관한 상담이 필요한 경우에는 정보통신부 산하 공공기관인 한국인터넷진흥원(KISA) 개인정보침해신고센터 또는 경찰청 사이버테러대응센터로 문의하시기 바랍니다.
