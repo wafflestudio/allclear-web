cafe152ce7005aaf3013f568d23ee6f75cc20d6d
kimnamheeee <nami200284@snu.ac.kr>
2026-05-03 15:20:52 +0900

    feat: add terms agreement api

--- server/service/terms.service.ts
+++ server/service/terms.service.ts
@@ -2,6 +2,7 @@ import { Repository } from 'typeorm'
 import { InjectRepository, Service } from '../provider'
 import { TermsAgreementEntity, TermsEntity } from '../infra/database/entities'
 import { Terms, toTermsDomain } from '../domain/model/Terms'
+import { BadRequestError } from '../domain/error'
 
 @Service
 export class TermsService {
@@ -29,5 +30,38 @@ export class TermsService {
 
     const entities = await query.getMany()
     return entities.map(toTermsDomain)
+  }
+
+  async agreeToTerms(userId: string, termUuids: string[]): Promise<void> {
+    const uniqueTermUuids = [...new Set(termUuids)]
+
+    const terms = await this.termsRepository.find({
+      where: uniqueTermUuids.map((uuid) => ({
+        uuid,
+        active: true,
+      })),
+    })
+
+    if (terms.length !== uniqueTermUuids.length) {
+      throw new BadRequestError('invalid term uuids')
+    }
+
+    if (terms.length === 0) {
+      return
+    }
+
+    await this.termsAgreementRepository
+      .createQueryBuilder()
+      .insert()
+      .into(TermsAgreementEntity)
+      .values(
+        terms.map((terms) => ({
+          userId,
+          termsId: terms.id,
+          agreedAt: new Date().toISOString(),
+        })),
+      )
+      .orIgnore()
+      .execute()
   }
 }
