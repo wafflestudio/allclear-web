b7e8ffa03785413c15cee8ea543b4b1160101b7b
kimnamheeee <nami200284@snu.ac.kr>
2026-05-03 15:21:15 +0900

    feat: add swagger docs

--- src/lib/schemas/announcements.ts
+++ src/lib/schemas/announcements.ts
@@ -1,6 +1,12 @@
 import { z } from 'src/lib/schemas/zod'
 import { AnnouncementSchema } from './common'
 
+export const DismissAnnouncementsSchema = z
+  .object({
+    announcementUuids: z.array(z.string().uuid()),
+  })
+  .openapi('DismissAnnouncements')
+
 export const AnnouncementsResponseSchema = z
   .object({
     data: z.array(AnnouncementSchema),
--- src/lib/schemas/terms.ts
+++ src/lib/schemas/terms.ts
@@ -1,6 +1,12 @@
 import { z } from 'src/lib/schemas/zod'
 import { TermsSchema } from './common'
 
+export const AgreeTermsSchema = z
+  .object({
+    termUuids: z.array(z.string().uuid()),
+  })
+  .openapi('AgreeTerms')
+
 export const TermsResponseSchema = z
   .object({
     data: z.array(TermsSchema),
